412ec6b72800f169a1d5df909ad72b2a12e5a5fb
MIsha <goody-man@bk.ru>
2021-12-08 13:06:47 +0500

    first

---
+++ js/tabs.js
@@ -0,0 +1,29 @@
+const tabs = (Option) => {
+   const {
+    selectorTabsButtons,
+    activeClassButton,
+    selectorTabsElement,
+    activeClassTab,
+    callback = () => {},
+   } = Option;
+   
+   const tabsBtns = document.querySelectorAll(selectorTabsButtons);
+   const tabsElem = document.querySelectorAll(selectorTabsElement);
+   tabsBtns.forEach((btn) => {
+       btn.addEventListener('click', () => {
+           for (let i = 0; i < tabsBtns.length; i++) {
+               if (tabsBtns [i] === btn) {
+                  tabsBtns[i].classList.add(activeClassButton);
+                  
+                  tabsElem[i].classList.add(activeClassTab);
+               }else {
+                tabsBtns[i].classList.remove(activeClassButton);
+                tabsElem[i].classList.remove(activeClassTab);
+               }
+              
+           }
+        });
+   });
+};
+
+export default tabs;
